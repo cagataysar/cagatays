a7ead27df009a3c7ca62e7666b415b8c35134b7d
Çağatay Sarıoğlan <sarioglancgty@outlook.com>
2023-12-07 11:35:40 +0300

    create project

---
+++ src/main/java/com/cagataysar/HomeController.java
@@ -0,0 +1,17 @@
+package com.cagataysar;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class HomeController {
+
+    @GetMapping("/home")
+    public String getIndex() {
+        return "index";
+    }
+
+
+}
